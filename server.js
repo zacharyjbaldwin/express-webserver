6a83ae9ea51c84cdddbf362ee4b7c10c60ca1cd5
Zachary J. Baldwin <zacharyjbaldwin@gmail.com>
2017-10-19 19:55:59 -0500

    Added projects page

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        tabTitle: 'Projects'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         error_message: 'Unable to handle this request.',
